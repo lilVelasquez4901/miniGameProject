e06330884537b20e35d599e8fbb3015664209925
SaisahPark1 <jpark277@west-mec.org>
2025-11-19 18:28:07 -0700

    Attempted to get started
    THIS JUST BREAKS

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 function playGame(){
-    const button = document.getElementById("button");
-    button.x = Math.ceil(Math.random()*window.innerWidth);
-    button.y = Math.ceil(Math.random()*window.innerHeight);
+    let wait = 0;
+    let clicked = false;
+    const target = document.getElementById("button");
+    target.clientX = Math.ceil(Math.random()*window.innerWidth);
+    target.clientY = Math.ceil(Math.random()*window.innerHeight);
+    target.addEventListener("click",(e) => {
+        clicked = true;
+    });
+    let hits = 0;
     while(hits < 20){
-        button.addEventListener("click",(e) => {
+        if (clicked){
+            console.log("CLLLIIICK");
             while(wait < 100){
-                wait++
+                wait++;
+                console.log("waiting");
             }
             button.x = Math.ceil(Math.random()*window.innerWidth);
             button.y = Math.ceil(Math.random()*window.innerHeight);
             hits++;
-        });
+            clicked = false;
+        }
     }
 }
 
-document.addEventListener("click",(e) => {
+document.getElementById("start").addEventListener("click",(e) => {
+    document.getElementById("start").style.display = "none";
     playGame();
 });
