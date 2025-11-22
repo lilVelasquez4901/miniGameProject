0e8b180d7f7a4179618d923b6e3a693d252a1965
SaisahPark1 <jpark277@west-mec.org>
2025-11-21 17:43:24 -0700

    Results, adjusting, resetting!
    I made the results update at the end of a game. At the start, the user can choose respawn times and game length. Finally I made it so it resets after a game.

--- script.js
+++ script.js
@@ -1,42 +1,77 @@
-let score = 0;
+let score;
 let startT;
 let endT;
-const target = document.getElementById("button");
+let targets;
+let waitTime;
+let totalClicks = 0;
+const target = document.getElementById("target");
 function check4win(){
     endT = Date.now();
-    if(score >= 11){
+    if(score >= targets){
         endGame();
     } else {
         score++;
         target.style.visibility = "hidden";
-        setTimeout(teleport, 500);
+        setTimeout(teleport, waitTime);
         allTimes.push(endT-startT);
     }
 }
-allTimes = [];
+let allTimes = [];
 function teleport(){
-    target.style.top = Math.ceil(Math.random()*1300)+"px";
+    target.style.top = Math.ceil(Math.random()*800)+"px";
     target.style.left = Math.ceil(Math.random()*1300)+"px";
     target.style.visibility = "visible";
     startT = Date.now();
 }
 
 function playGame(){
+    score = 0;
+    totalClicks = 0;
+    allTimes.length = 0;
+    let numRadio = document.getElementsByName('selectTargetAmount');
+    if (numRadio[0].checked){
+        targets = 6;
+    } else if (numRadio[2].checked){
+        targets = 21;
+    } else {
+        targets = 11;
+    }
+    let timeRadio = document.getElementsByName('selectSpawnDelay');
+    if (timeRadio[0].checked){
+        waitTime = 0;
+    } else if (timeRadio[2].checked){
+        waitTime = 2000;
+    } else {
+        waitTime = 1000;
+    }
     target.addEventListener("click", check4win);
-    document.addEventListener("click",() => {
-        totalClicks++;
-    });
+    target.style.visibility = "visible";
+    document.addEventListener("click", addClick);
 }
-let totalClicks = 0;
+function addClick(){
+    totalClicks++;
+}
 function endGame(){
     target.removeEventListener("click", check4win);
+    document.removeEventListener("click", addClick);
     allTimes.shift();
-    let average = (allTimes[0]+allTimes[1]+allTimes[2]+allTimes[3]+allTimes[4]+allTimes[5]+allTimes[6]+allTimes[7]+allTimes[8]+allTimes[9])/10;
+    let average = 0;
+    for (i=0;i<(targets-1);i++){
+        average += allTimes[i];
+    }
+    average /= (targets-1);
     document.getElementById("reactionTime").innerHTML = `Your average reaction time was ${average/1000} seconds`;
-    document.getElementById("missedClicks").innerHTML = `You missed ${totalClicks-12} clicks`;
+    document.getElementById("reactionTime").style.visibility = "visible";
+    document.getElementById("missedClicks").innerHTML = `You missed ${totalClicks-targets-1} clicks`;
+    document.getElementById("missedClicks").style.visibility = "visible";
+
+    document.getElementById("start").style.visibility = "visible";
+    target.style.visibility = "hidden";
+    target.style.top = "50%";
+    target.style.top = "50%";
 }
 
 document.getElementById("start").addEventListener("click",() => {
-    document.getElementById("start").style.display = "none";
+    document.getElementById("start").style.visibility = "hidden";
     playGame();
 });
