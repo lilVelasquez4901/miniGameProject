681f6620ae11bb82e508400cbd10676614676239
lilVelasquez4901 <evelas562@west-mec.org>
2025-11-30 16:30:20 -0700

    Finalizing
    Combined Josiah's scripting with my more CSS focused script to finalize the game. Should be in working functional order with small nitpicks but otherwise perfectly fine.

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-// function playGame(){
-//     let wait = 0;
-//     let clicked = false;
-//     const target = document.getElementById("button");
-//     target.clientX = Math.ceil(Math.random()*window.innerWidth);
-//     target.clientY = Math.ceil(Math.random()*window.innerHeight);
-//     target.addEventListener("click",(e) => {
-//         clicked = true;
-//     });
-//     let hits = 0;
-//     while(hits < 20){
-//         if (clicked){
-//             console.log("CLLLIIICK");
-//             while(wait < 100){
-//                 wait++;
-//                 console.log("waiting");
-//             }
-//             button.x = Math.ceil(Math.random()*window.innerWidth);
-//             button.y = Math.ceil(Math.random()*window.innerHeight);
-//             hits++;
-//             clicked = false;
-//         }
-//     }
-// }
-
-// document.getElementById("start").addEventListener("click",(e) => {
-//     document.getElementById("start").style.display = "none";
-//     playGame();
-// });
-let isStarted = false;
-let clicked = false;
+// Esaih's coding
 let body = document.body;
 let textColor1 = '#ece586';
 let textColor2 = '#545353';
@@ -79,6 +49,7 @@ themeSwapper.addEventListener('click', e=>{
         label6.style.color = textColor1;
         misclicks.style.color = textColor1;
         reactionTiming.style.color = textColor1;
+        target.style.background = textColor1;
     } else {
         themeSwapper.textContent = 'Dark Mode'
         body.style.background = backgroundColors[defaultBackground + 1]
@@ -96,24 +67,104 @@ themeSwapper.addEventListener('click', e=>{
         label6.style.color = textColor2;
         misclicks.style.color = textColor2;
         reactionTiming.style.color = textColor2;
+        target.style.background = textColor2;
     };
 })
 theme.addEventListener('click', themeSwap)
-function beginning(){
-    startButton.addEventListener("click",(e)=>{
-        clicked = true;
-        isStarted = true;
-    });
-};
 function revealingText(){
     startButton.addEventListener("click", (e)=>{
         infoText.style.visibility = 'hidden';
         spawnMenu.style.visibility = 'hidden';
         targetMenu.style.visibility = 'hidden';
         targetsRemain.style.visibility = 'visible';
-        targetsRemain.textContent = `Targets remaining:`
     });
 }
+// Josiah's coding
+let score;
+let startT;
+let endT;
+let targets;
+let waitTime;
+let totalClicks = 0;
+let remainTargets
+const target = document.getElementById("target");
+function check4win(){
+    // PUT THE TARGET CLICK SOUND HERE!
+    endT = Date.now();
+    if(score >= targets){
+        endGame();
+    } else {
+        score++;
+        target.style.visibility = "hidden";
+        setTimeout(teleport, waitTime);
+        allTimes.push(endT-startT);
+        remainTargets--;
+        document.getElementById("targetsRemaining").innerHTML = `Targets Remaining: ${remainTargets}`;
+    }
+}
+let allTimes = [];
+function teleport(){
+    target.style.top = Math.ceil(Math.random()*800)+"px";
+    target.style.left = Math.ceil(Math.random()*1300)+"px";
+    target.style.visibility = "visible";
+    startT = Date.now();
+}
+
+function playGame(){
+    // MAKE THE UI DISSAPPEAR
+    score = 0;
+    totalClicks = 0;
+    allTimes.length = 0;
+    let numRadio = document.getElementsByName('selectTargetAmount');
+    if (numRadio[0].checked){
+        targets = 6;
+    } else if (numRadio[2].checked){
+        targets = 21;
+    } else {
+        targets = 11;
+    }
+    remainTargets = targets
+    let timeRadio = document.getElementsByName('selectSpawnDelay');
+    if (timeRadio[0].checked){
+        waitTime = 0;
+    } else if (timeRadio[2].checked){
+        waitTime = 2000;
+    } else {
+        waitTime = 1000;
+    }
+    target.addEventListener("click", check4win);
+    target.style.visibility = "visible";
+    document.addEventListener("click", addClick);
+}
+function addClick(){
+    totalClicks++;
+    // OPTIONAL: a regular click sound effect
+}
+function endGame(){
+    // GAME COMPLETED music thing
+    target.removeEventListener("click", check4win);
+    document.removeEventListener("click", addClick);
+    allTimes.shift();
+    let average = 0;
+    for (i=0;i<(targets-1);i++){
+        average += allTimes[i];
+    }
+    average /= (targets-1);
+    document.getElementById("reactionTime").innerHTML = `Your average reaction time was ${average/1000} seconds`;
+    document.getElementById("reactionTime").style.visibility = "visible";
+    document.getElementById("missedClicks").innerHTML = `You missed ${totalClicks-targets-1} clicks`;
+    document.getElementById("missedClicks").style.visibility = "visible";
+    // MAKE UI REAPPEAR
+
+    document.getElementById("start").style.visibility = "visible";
+    target.style.visibility = "hidden";
+    target.style.top = "50%";
+    target.style.left = "50%";
+}
+
+document.getElementById("start").addEventListener("click",() => {
+    document.getElementById("start").style.visibility = "hidden";
+    playGame();
+});
 themeSwap()
-beginning()
 revealingText()
